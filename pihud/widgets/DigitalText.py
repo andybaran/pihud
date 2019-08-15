1c7248bbd91bf7026c48d9ab304614e85f7de98f
Denys Riaguzov <d.riaguzov@levi9.com>
2019-08-15 18:12:45 +0300

    DigitalText

--- pihud/widgets/DigitalText.py
+++ pihud/widgets/DigitalText.py
@@ -18,23 +18,11 @@ class DigitalText(QWidget):
         #print [str(f) for f in self.families] #DS-Digital
 
         self.font         = QFont("DS-Digital")
-        self.note_font    = QFont("DS-Digital")
-        self.title_font   = QFont("DS-Digital")
         self.color        = QColor(config["color"])
-        self.pen_color    = QColor(Qt.black)
         self.red_color    = QColor(config["redline_color"])
-        self.brush        = QBrush(self.color)
-        self.brush_bg     = QBrush(QColor("#222222"))
-        self.brush_red    = QBrush(self.red_color)
-        self.brush_red_bg = QBrush(QColor("#332222"))
-        self.pen          = QPen(self.pen_color)
-        self.red_pen      = QPen(self.red_color)
-        self.text_pen     = QPen(self.color)
+        self.pen          = QPen(self.color)
 
-        #self.font.setPixelSize(self.config["font_size"])
-        self.font.setPixelSize(int(max(self.width(), self.height())/1.5))
-        self.note_font.setPixelSize(int(max(self.width(), self.height())/10))
-        self.title_font.setPixelSize(int(max(self.width(), self.height())/7))
+        self.font.setPixelSize(self.config["font_size"])
 
         self.red_value = config["redline"]
         if self.red_value is None:
@@ -53,20 +41,25 @@ class DigitalText(QWidget):
         painter = QPainter()
         painter.begin(self)
 
-        self.t_height = self.config["font_size"] + 8
+        font_size = int(max(self.width(), self.height())/3)
+        title_font_size = int(font_size/3)
+        self.t_height = self.height() - title_font_size - 8
 
         painter.setFont(self.font)
         painter.setPen(self.pen)
-        painter.setRenderHint(QPainter.Antialiasing)
-
-        h = 0
+        #painter.setRenderHint(QPainter.Antialiasing)
 
         if len(self.config["title"]) > 0:
-            h += self.t_height
-            r = QRect(0, 0, self.width(), self.t_height)
-            painter.drawText(r, Qt.AlignVCenter, self.config["title"])
+            r = QRect(0, self.t_height, self.width(), self.height())
+            #print(0, self.t_height, self.width(), self.height())
+            #self.font.setPixelSize(title_font_size)
+            painter.drawRect(r)
+            #painter.drawText(r, Qt.AlignCenter, self.config["title"])
 
-        r = QRect(0, h, self.width(), self.t_height)
-        painter.drawText(r, Qt.AlignCenter, str(int(round(self.value))))
+        self.font.setPixelSize(font_size)
+        r = QRect(0, 0, self.width(),  self.t_height)
+        #painter.drawRect(r)
+        #print(0, 0, self.width(),  self.t_height)
+        #painter.drawText(r, Qt.AlignCenter, str(int(round(self.value))))
 
         painter.end()
