3a5a46e332c4c185e23836ae161cd6b90565c074
andybaran <andy@andybaran.net>
2020-05-30 09:18:26 -0400

    files for easy install:

--- pihud-installer.sh
+++ pihud-installer.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 echo "Pulling down python3 apt packages"
-sudo apt-get install vim python3-pyqt5 python3-pip python3-yaml python3-rpi.gpio playerctl -y
+sudo apt-get install vim python3-pyqt5 python3-pip python3-yaml python3-rpi.gpio -y
 
 echo "Doing pips for modules without packages"
-pip3 install obd
+pip3 install obd board busio adafruit_mprls smbus2 rpi.bme280 adafruit-circuitpython-mprls 
 
 echo "Cloning code from GitHub"
 git clone https://github.com/Ircama/ELM327-emulator
-git clone https://github.com/andybaran/pihud.git
+#git clone https://github.com/andybaran/pihud.git
 git clone  https://github.com/andybaran/rpi-audio-receiver
 
+echo "Setup systemd"
+cp ./configs/systemd/pihud.service /lib/systemd/system/pihud.service
+systemctl daemon-reload
+systemctl enable pihud.service
+systemctl start pihud.service
