4ad68fc2e7392525899b6f826642a48a9bd3dd9e
andybaran <andy@andybaran.net>
2020-06-06 09:43:43 -0400

    messing with saving pages

--- pihud/GlobalConfig.py
+++ pihud/GlobalConfig.py
@@ -101,12 +101,12 @@ class GlobalConfig():
                     try:
                         config.data()
                     except:
-                        print("type : ", type(config))
+                        print("except type : ", type(config))
                         print(config)
                         current_page.append(config)
                     else:
-                        print("type : ", type(config.data()))
-                        print(config.data())
+                        print("else type : ", type(config.Config.data()))
+                        print(config.Config.data())
                         current_page.append(config.data())
 
                 pages.append(current_page)
