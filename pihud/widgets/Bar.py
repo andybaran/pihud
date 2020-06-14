7e626be8bc91553b6ff40552e2d537d0fbdb7a7f
andybaran <andy@andybaran.net>
2020-06-14 19:34:09 -0400

    minor

--- pihud/widgets/Bar.py
+++ pihud/widgets/Bar.py
@@ -33,6 +33,7 @@ class Bar_Horizontal(QWidget):
 
     def render(self, response):
         # approach the value
+        if
         self.value += (response.value.magnitude - self.value) / 4
         self.update()
 
